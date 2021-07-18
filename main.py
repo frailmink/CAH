3afdf1e921554ff5d704a98af9b5f3fe4b3fe686
frailmink <adrismithgon@gmail.com>
2021-07-18 22:39:49 +0100

    added comments to the back and front end

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# run this with uvicorn app:main and go to http://localhost:8000
+# for testing go to http://localhost:8000/docs
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import sqlite3
@@ -9,8 +12,9 @@ app = FastAPI()
 
 @app.get("/answer_card/{card_id}")
 async def answer_card(card_id):
-    for card in cur.execute(f"SELECT * FROM AnswerCards WHERE id = {card_id}"):
-        break
+    # returns the answer card with id card_id
+    card = cur.execute(f"SELECT * FROM AnswerCards WHERE id = {card_id}").fetchone()
+    # returns the card in the form of a dictionary
     return {
         "id": card[0],
         "text": card[1],
@@ -18,8 +22,9 @@ async def answer_card(card_id):
 
 @app.get("/question_card/{card_id}")
 async def question_card(card_id):
-    for card in cur.execute(f"SELECT * FROM QuestionCards WHERE id = {card_id}"):
-        break
+    # returns the question card with id card_id
+    card = cur.execute(f"SELECT * FROM QuestionCards WHERE id = {card_id}").fetchone()
+    # returns the card in the form of a dictionary
     return {
         "id": card[0],
         "text": card[1],
@@ -28,16 +33,21 @@ async def question_card(card_id):
 
 @app.get("/num_answer_cards")
 async def num_answer_cards():
-    for count in cur.execute("SELECT COUNT() FROM AnswerCards"):
-        break
+    # returns the maximum number of answer cards in the db
+    # gets the max num in the form of a list
+    count = cur.execute("SELECT COUNT() FROM AnswerCards").fetchone()
+    # returns the value of the list
     return count[0]
 
 @app.get("/num_question_cards")
 async def num_question_cards():
-    for count in cur.execute("SELECT COUNT() FROM QuestionCards"):
-        break
+    # returns the maximum number of question cards in the db
+    # gets the max num in the form of a list 
+    count = cur.execute("SELECT COUNT() FROM QuestionCards").fetchone()
+    # returns the value of the list
     return count[0]
 
 @app.get("/")
 async def root():
+    #returns the front end code so that it can be run
     return HTMLResponse(open("index.html", "rt").read())
