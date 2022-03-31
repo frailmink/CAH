5e2037bf2d242f6ae381e59ef2643d16f5b95a3e
frailmink <adrismithgon@gmail.com>
2022-03-31 19:36:18 +0100

    working game
    Finished the game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # run this with uvicorn main:app and go to http://localhost:8000
 # for testing go to http://localhost:8000/docs
 
+from ctypes import pointer
+import string
 from typing import Awaitable
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
@@ -34,6 +36,41 @@ async def question_card(card_id: int):
         "num_gaps": card[2]
     }
 
+@app.post("/reset_choices/{game_id}")
+async def reset_choices(game_id: int):
+    list_players = cur.execute(f"SELECT ListPlayerID FROM Games WHERE id = {game_id}").fetchone()[0]
+    list_players = json.loads(list_players)
+    for players in list_players:
+        cur.execute(f"UPDATE Players SET choice = '(NULL)' WHERE id = {players}")
+    con.commit()
+
+@app.post("/change_czar/{game_id}")
+async def change_czar(game_id: int):
+    count = 0
+    czar_id = cur.execute(f"SELECT CzarID FROM Games WHERE id = {game_id}").fetchone()[0]
+    list_players = cur.execute(f"SELECT ListPlayerID FROM Games WHERE id = {game_id}").fetchone()[0]
+    list_players = json.loads(list_players)
+    while czar_id != list_players[count]:
+        count += 1
+    if count == len(list_players) - 1:
+        cur.execute(f"UPDATE Games SET CzarID = {list_players[0]} WHERE id = {game_id}")
+    else:
+        cur.execute(f"UPDATE Games SET CzarID = {list_players[count + 1]} WHERE id = {game_id}")
+    con.commit()
+
+@app.post("/remove_chosen_cards/{player_id}/{cards_chosen}")
+async def remove_chosen_cards(player_id: int, cards_chosen: str):
+    list_cards = cur.execute(f"SELECT ListCards FROM Players WHERE id = {player_id}").fetchone()[0]
+    list_cards = json.loads(list_cards)
+    cards_chosen = json.loads(cards_chosen)
+    choice_ids = []
+    for index in cards_chosen:
+        choice_ids.append(list_cards[index - 1])
+    for card_id in choice_ids:
+        list_cards.remove(card_id)
+    cur.execute(f"UPDATE Players SET ListCards = '{list_cards}' WHERE id = {player_id}")
+    con.commit()
+
 @app.post("/deal_qcard/{game_id}")
 async def deal_qcard(game_id: int):
     keep_going = True
@@ -45,7 +82,33 @@ async def deal_qcard(game_id: int):
             keep_going = False
     cur.execute(f"UPDATE Games SET QCard = '{rand_int}' WHERE id = {game_id}")
     q_card = await question_card(rand_int)
-    return q_card["text"]
+    return q_card["text"], q_card["num_gaps"]
+
+@app.get("/check_game_finish/{game_id}/{points_to_win}")
+async def check_game_finish(game_id: int, points_to_win: int):
+    # checks if the game has finished
+    # returns a true or false statement depending on whether the game has ended or not, as well as, the id of the winner if there was a winner
+    winner_id = 0
+    game_ended = False
+    list_players = cur.execute(f"SELECT ListPlayerID FROM Games WHERE id = {game_id}").fetchone()[0]
+    list_players = json.loads(list_players)
+    #loops through all the players and checks if they have enoough points to win
+    for player in list_players:
+        points = cur.execute(f"SELECT points FROM Players WHERE id = {player}").fetchone()[0]
+        #checks to see whether the player has won the game
+        if points == points_to_win:
+            game_ended = True
+            winner_id = player
+    #if any player has enough points to win it returns a true statament and if no one does it returns a false statement
+    return game_ended, winner_id
+
+@app.get("/num_points/{player_id}")
+async def num_points(player_id: int):
+    # returns the number of points the user with id player_id has
+    # this function is going to be used in the frontend to check if the player has won the game
+    points = cur.execute(f"SELECT points FROM Players WHERE id = {player_id}").fetchone()[0]
+    # returns the points the user has
+    return points
 
 @app.get("/num_answer_cards")
 async def num_answer_cards():
@@ -120,11 +183,25 @@ async def deal_cards(player_id: int):
 
 @app.post("/create_game")
 async def create_game():
-    cur.execute(f"INSERT INTO Games (ListPlayerID) VALUES ('[]')")
+    cur.execute(f"INSERT INTO Games (ListPlayerID, EveryoneIn) VALUES ('[]', False)")
     game_id = cur.lastrowid
     q_card = await deal_qcard(game_id)
     con.commit()
     return game_id, q_card
+
+@app.post("/everyone_in/{game_id}")
+async def everyone_in(game_id: int):
+    cur.execute(f"UPDATE Games SET EveryoneIn = True WHERE id = {game_id}")
+    con.commit()
+
+@app.get("/check_EveryoneIn/{game_id}")
+async def check_EveryoneIn(game_id: int):
+    everyone_in = cur.execute(f"SELECT EveryoneIn FROM Games WHERE id = {game_id}").fetchone()[0]
+    if everyone_in == 0:
+        everyone_in = False
+    else:
+        everyone_in = True
+    return everyone_in
 
 @app.post("/join_game/{game_id}/{player_id}")
 async def join_game(player_id: int, game_id: int):
@@ -205,11 +282,11 @@ async def give_points(czar_id: int, point_winner: int):
 
 @app.get("/return_czar_choice/{game_id}")
 async def return_czar_choice(game_id: int):
-    choice_in = False
+    choice_in = True
     czar_id = cur.execute(f"SELECT CzarID FROM Games WHERE id = {game_id}").fetchone()[0]
     czar_choice = cur.execute(f"SELECT choice FROM Players WHERE id = {czar_id}").fetchone()[0]
-    if czar_choice != None:
-        choice_in = True
+    if czar_choice == None or czar_choice == "(NULL)":
+        choice_in = False
     return choice_in, czar_choice
 
 @app.get("/")
