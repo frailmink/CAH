9f92ec4d72428e4f0127db867882859300646b5f
frailmink <adrismithgon@gmail.com>
2022-03-31 22:19:56 +0100

    added comments

--- main.py
+++ main.py
@@ -38,20 +38,26 @@ async def question_card(card_id: int):
 
 @app.post("/reset_choices/{game_id}")
 async def reset_choices(game_id: int):
+    # resets the choices of the players in the db by giving them a (NULL) value
     list_players = cur.execute(f"SELECT ListPlayerID FROM Games WHERE id = {game_id}").fetchone()[0]
     list_players = json.loads(list_players)
+    # loops through all the players and sets their choices to a null value
     for players in list_players:
         cur.execute(f"UPDATE Players SET choice = '(NULL)' WHERE id = {players}")
     con.commit()
 
 @app.post("/change_czar/{game_id}")
 async def change_czar(game_id: int):
+    # changes the czar
     count = 0
+    # gets previous czar
     czar_id = cur.execute(f"SELECT CzarID FROM Games WHERE id = {game_id}").fetchone()[0]
     list_players = cur.execute(f"SELECT ListPlayerID FROM Games WHERE id = {game_id}").fetchone()[0]
     list_players = json.loads(list_players)
+    # finds the location of the czar's id in the list ListPlayers from the db 
     while czar_id != list_players[count]:
         count += 1
+    # checks if the last czar was at the end of the list and if they were it sets the next czar to be the first one in the list
     if count == len(list_players) - 1:
         cur.execute(f"UPDATE Games SET CzarID = {list_players[0]} WHERE id = {game_id}")
     else:
@@ -60,12 +66,15 @@ async def change_czar(game_id: int):
 
 @app.post("/remove_chosen_cards/{player_id}/{cards_chosen}")
 async def remove_chosen_cards(player_id: int, cards_chosen: str):
+    # removes the chosen answer cards of the players
     list_cards = cur.execute(f"SELECT ListCards FROM Players WHERE id = {player_id}").fetchone()[0]
     list_cards = json.loads(list_cards)
     cards_chosen = json.loads(cards_chosen)
     choice_ids = []
+    # due to the fact that it only returns the location of the cards chosen this loops through them and appends the card ids to a list
     for index in cards_chosen:
         choice_ids.append(list_cards[index - 1])
+    # loops through the answer card ids and removes them from the list of cards the player has from the db
     for card_id in choice_ids:
         list_cards.remove(card_id)
     cur.execute(f"UPDATE Players SET ListCards = '{list_cards}' WHERE id = {player_id}")
@@ -73,13 +82,19 @@ async def remove_chosen_cards(player_id: int, cards_chosen: str):
 
 @app.post("/deal_qcard/{game_id}")
 async def deal_qcard(game_id: int):
+    # changes the question card to a new one
     keep_going = True
+    # gets number of question cards in the db
     max_qcard = await num_question_cards()
     last_qcard = cur.execute(f"SELECT QCard FROM Games WHERE id = {game_id}").fetchone()[0]
+    # loops indefently until the new question card is not the same as the old question card
     while keep_going:
+        # gets random number
         rand_int = random.randint(1, max_qcard)
+        # checks if the random number is the same number as the old question card id
         if rand_int != last_qcard:
             keep_going = False
+    # gets the question card from the db with the id gotten from the random number
     cur.execute(f"UPDATE Games SET QCard = '{rand_int}' WHERE id = {game_id}")
     q_card = await question_card(rand_int)
     return q_card["text"], q_card["num_gaps"]
@@ -128,6 +143,7 @@ async def num_question_cards():
 
 @app.get("/czar/{game_id}/{player_id}") 
 async def czar(game_id: int, player_id: int):
+    # checks if the player is the current czar
     czar_id = cur.execute(f"SELECT CzarID FROM Games WHERE id = {game_id}").fetchone()[0]
     if czar_id == player_id:
         Czar = True
@@ -137,66 +153,85 @@ async def czar(game_id: int, player_id: int):
 
 @app.post("/create_player")
 async def create_player():
+    # creates a new player in the db
     cur.execute("INSERT INTO Players (ListCards, points) VALUES ('[]', 0)")
     con.commit()
     return cur.lastrowid
 
 @app.post("/return_cards/{player_id}")
 async def return_cards(player_id: int):
+    # returns the answer cards in the players hand from the db
     PlayersCards = cur.execute(f"SELECT ListCards FROM Players WHERE id = {player_id}").fetchone()
     return PlayersCards[0]
 
 @app.post("/deal_cards/{player_id}")
 async def deal_cards(player_id: int):
+    # deals the needed number of cards to get the players hand to 7 cards
     max_cards = 7
     num_players = []
     number_cards = 0
     temp_list = []
     player_id = int(player_id)
+    # gets the maximum number of answer cards
     max_a_cards = await num_answer_cards()
+    # loops through all the players in the db and appends their ids to a list
     for index in cur.execute("SELECT id FROM Players"):
         num_players.append(index[0])
     unused_cards = []
+    # loops through all the answer cards and makes a list of all the possible answer cards that have not been used
     for index in range(max_a_cards):
         unused_cards.append(index + 1)
+    # loops through the player ids and removes the answer cards taht are in the player's hand
     for ids in num_players:
         list_cards = cur.execute(f"SELECT ListCards FROM Players WHERE id = {ids}").fetchone()[0]
         list_cards = json.loads(list_cards)
+        # loops through all the cards in the player with player id ids and removes the answer cards in their hand from the list
         for cards in list_cards:
             unused_cards.remove(cards)
+            # checks to see if the current id is the players id and counts how many answer cards they currently have in their hand
             if (ids) == player_id:
                 number_cards += 1
     list_cards = cur.execute(f"SELECT ListCards FROM Players WHERE id = {player_id}").fetchone()[0]
     list_cards = json.loads(list_cards)
+    # loops until the player has the 7 cards in their hand and adds a random card into their hand
     for index in range(max_cards - number_cards):
         random_num = random.randint(1, len(unused_cards))
+        # adds the random answer card id to the players hand
         list_cards.append(unused_cards[random_num])
+        # removes the chosen answer card from the list of unused answer cards
         unused_cards.pop(random_num)
     cur.execute(f"UPDATE Players SET ListCards = '{list_cards}' WHERE id = {player_id}")
+    # loops through all the answer card ids and turns them into the text equivilant to return it to the frontend
     for card_id in list_cards:
         acard = await answer_card(card_id)
         acard = acard["text"]
         temp_list.append(acard)
     list_cards = temp_list
     con.commit()
+    # returns the list of answer card's text in the players hand
     return list_cards
 
 @app.post("/create_game")
 async def create_game():
+    # creates a game in the db
     cur.execute(f"INSERT INTO Games (ListPlayerID, EveryoneIn) VALUES ('[]', False)")
     game_id = cur.lastrowid
+    # gets a new question card and saves in the db
     q_card = await deal_qcard(game_id)
     con.commit()
     return game_id, q_card
 
 @app.post("/everyone_in/{game_id}")
 async def everyone_in(game_id: int):
+    # sets the property EveryoneIn in the db to true
     cur.execute(f"UPDATE Games SET EveryoneIn = True WHERE id = {game_id}")
     con.commit()
 
 @app.get("/check_EveryoneIn/{game_id}")
 async def check_EveryoneIn(game_id: int):
+    # checks if the property EveryoneIn in the db is true or false
     everyone_in = cur.execute(f"SELECT EveryoneIn FROM Games WHERE id = {game_id}").fetchone()[0]
+    # booleans in a db are stored as 1 (true) or a 0 (false)
     if everyone_in == 0:
         everyone_in = False
     else:
@@ -205,12 +240,14 @@ async def check_EveryoneIn(game_id: int):
 
 @app.post("/join_game/{game_id}/{player_id}")
 async def join_game(player_id: int, game_id: int):
+    # saves the players id into the list of players in the db
+    # checks if there is no game in the db and if there isnt it creates a new one
     try:
         games = cur.execute(f"SELECT * FROM Games").fetchone()[0]
     except:
         game_id = await create_game()
         game_id = game_id[0]
-
+    # saves the player's id into the list of players in the db
     list_players = cur.execute(f"SELECT ListPlayerID FROM Games WHERE id = {game_id}").fetchone()[0]
     list_players = json.loads(list_players)
     list_players.append(player_id)
@@ -220,33 +257,40 @@ async def join_game(player_id: int, game_id: int):
 
 @app.post("/save_choice/{player_id}/{choice}")
 async def save_choice(player_id: int, choice: str):
+    # saves the list of answer cards chosen by the player in the db
     choice = json.loads(choice)
     player_cards = cur.execute(f"SELECT ListCards FROM Players WHERE id = {player_id}").fetchone()[0]
     player_cards = json.loads(player_cards)
     choice_ids = []
+    # loops thorugh the choices and gets their respective ids and saves it to a list
     for index in choice:
         choice_ids.append(player_cards[index - 1])
+    # saves the list in the db
     cur.execute(f"UPDATE Players SET choice = '{choice_ids}' WHERE id = {player_id}")
     con.commit()
 
 @app.post("/delete_game/{game_id}")
 async def delete_game(game_id: int):
+    # deletes the game with id game_id from the db
     cur.execute(f"DELETE FROM Games WHERE id = {game_id}")
     con.commit()
 
 @app.post("/delete_player/{player_id}")
 async def delete_player(player_id: int):
+    # deletes the player with id player_id from the db
     cur.execute(f"DELETE FROM Players WHERE id = {player_id}")
     con.commit()
 
 @app.get("/return_num_players/{game_id}")
 async def return_num_players(game_id: int):
+    # returns the number of players in the game
     list_player_id = cur.execute(f"SELECT ListPlayerID FROM Games WHERE id = {game_id}").fetchone()[0]
     list_player_id = json.loads(list_player_id)
     return len(list_player_id)
 
 @app.get("/return_qcard/{game_id}")
 async def return_qcard(game_id: int):
+    # returns the question card from the db
     q_card_id = cur.execute(f"SELECT QCard FROM Games WHERE id = {game_id}").fetchone()[0]
     q_card = await question_card(q_card_id)
     num_gaps = q_card["num_gaps"]
@@ -255,24 +299,29 @@ async def return_qcard(game_id: int):
 
 @app.get("/return_choices/{game_id}")
 async def return_choices(game_id: int):
+    # checks if all the players have submitted their answer cards and if they have it returns those choices
     choice_in = True
     list_choices = []
     list_players = cur.execute(f"SELECT ListPlayerID FROM Games WHERE id = {game_id}").fetchone()[0]
     list_players = json.loads(list_players)
     czar_id = cur.execute(f"SELECT CzarID FROM Games WHERE id = {game_id}").fetchone()[0]
     list_players.remove(czar_id)
+    # loops through all the players and gets their answer cards
     for player_id in list_players:
         choice = cur.execute(f"SELECT choice FROM Players WHERE id = {player_id}").fetchone()[0]
+        # tries to save the answer cards into a list and if it doesnt work it means that the player has not submitted their answer card
         try:
             choice = json.loads(choice)
             list_choices.append((player_id, choice))
         except:
             choice_in = False
             break
+    # returns a false statement if the player has not submitted their answer card yet else it returns a true statement and the cards
     return choice_in, list_choices 
 
 @app.post("/give_points/{czar_id}/{point_winner}")
 async def give_points(czar_id: int, point_winner: int):
+    # gives a point to the player who was chosen by the czar
     cur.execute(f"UPDATE Players SET choice = '{point_winner}' WHERE id = {czar_id}")
     points = cur.execute(f"SELECT points FROM Players WHERE id = {point_winner}").fetchone()[0]
     points += 1
@@ -282,9 +331,12 @@ async def give_points(czar_id: int, point_winner: int):
 
 @app.get("/return_czar_choice/{game_id}")
 async def return_czar_choice(game_id: int):
+    # checks if the player has submitted their choice and if they have it returns it
     choice_in = True
     czar_id = cur.execute(f"SELECT CzarID FROM Games WHERE id = {game_id}").fetchone()[0]
     czar_choice = cur.execute(f"SELECT choice FROM Players WHERE id = {czar_id}").fetchone()[0]
+    # checks if the czar has chosen a set of cards
+    # the empty choices can either be (NULL) or None
     if czar_choice == None or czar_choice == "(NULL)":
         choice_in = False
     return choice_in, czar_choice
